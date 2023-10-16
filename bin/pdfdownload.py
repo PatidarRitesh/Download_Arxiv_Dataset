bf72b70fd4d9e57082a5bc813a6306f8d98b2731
Ritesh Patidar <112418113+PatidarRitesh@users.noreply.github.com>
2023-10-16 21:52:54 +0530

    Add files via upload

---
+++ bin/pdfdownload.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.insert(0, '/home/patidarritesh/ThesisWork4/arxiv-public-datasets/')
+from arxiv_public_data import s3_bulk_download
+if __name__ == '__main__':
+    out = sys.argv[1] if len(sys.argv) > 1 else None
+    manifest = s3_bulk_download.get_manifest(out)
+    # s3_bulk_download.process_manifest_files(manifest)
+    s3_bulk_download.download_check_tarfiles(manifest)
